3fdc2c4c4e352f858e3f2f0400abb5d621f22051
Mounir R'Quiba <mounir.rquiba@gmail.com>
2020-07-09 18:53:55 +0200

    Detect movement option into view next

--- src/app/_/templates/templates-modal/templates-modal-exercice-manager/templates-modal-exercice-manager.component.ts
+++ src/app/_/templates/templates-modal/templates-modal-exercice-manager/templates-modal-exercice-manager.component.ts
@@ -99,7 +99,7 @@ export class TemplatesModalExerciceManagerComponent  extends FormModalCore imple
 
     let movements = this.model.movements.map((movement) => {
       if (!movement.value) {
-        movement.value = 90;
+        movement.value = 0;
       }
 
       this.setUnitLabel(movement, 'unit', 'unit_label');
@@ -111,7 +111,7 @@ export class TemplatesModalExerciceManagerComponent  extends FormModalCore imple
           unit: parseInt('' + set.unit) || 1,
           set: set.set || 1,
           rep: set.rep || 1,
-          value: set.value || 90,
+          value: set.value || 0,
           unit_label: set.unit_label,
         };
       });
@@ -134,7 +134,7 @@ export class TemplatesModalExerciceManagerComponent  extends FormModalCore imple
             unit: parseInt('' + set.unit) || 1,
             set: set.set || 1,
             interval: set.interval || 1,
-            value: set.value || 90,
+            value: set.value || 0,
             unit_label: set.unit_label,
           };
         });
